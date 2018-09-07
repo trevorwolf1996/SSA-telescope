963a7cc13d017629e0ad95e36930211276e11791
trevorwolf1996 <trevorfrog321@gmail.com>
2018-09-07 15:06:01 -0400

    9-7-18

--- new_Pull_NOAA_F_10p7.py
+++ new_Pull_NOAA_F_10p7.py
@@ -2,20 +2,19 @@
 """
 This is an updated version of the pull_NOAA_F_10p7.py. This file will draw all
 of the values from a set of files updated a various time intervals by NOAA to
-find the values of F_10.7 and F_10.7_bar. This progam's purpose is to fill in the 
+find the values of F_10.7. This progam's purpose is to fill in the 
 gaps for each of the data sets in order to create one that spans the entire time 
-domain from any time in the past, until present. Because of the nature of how NOAA
-updates this flux value, there is not one data set that contains all the information 
-needed to do this. The two data sets needed are yyyy.vm and quar_dsd.txt. Numeric values
-of these files will then be converted to a numpy array to which the values will be 
-easily accessible for analysis. 
+domain from times within the last year and 45 days into the futyre. 
+Because of the nature of how NOAA updates this flux value, there is not one data 
+set that contains all the information needed to do this. 
 
 Inputs:
     yr: Year of interest 
     mon: Month of interest
 
 Outputs:
-    F_10p7_obs_array: 
+    F_10p7_obs_array: Array containing past observered F_10.7 flux values
+    F_10p7_adj_future_array: Array containing predicted adjusted F_10.7 flux values
 
 Copyright (c) Trevor Wolf 2018. All Rights Reserved. 
 """
@@ -23,6 +22,89 @@ import numpy as np
 import urllib.request
 
 def pull_NOAA_F_10p7(yr, mon):
+    
+    #
+    # Pull future information about the F_10.7 values
+    #
+    ftp_site = 'ftp://ftp.swpc.noaa.gov/pub/latest/45DF.txt';
+    #
+    month_vec = [b'Jan', b'Feb', b'Mar', b'Apr', b'May', b'Jun', b'Jul', b'Aug', b'Sep',\
+                 b'Oct', b'Nov', b'Dec'];
+    #
+    response = urllib.request.urlopen(ftp_site);
+    #
+    first_entry_flag = 1;
+    index = 0;
+    #
+    for line in reversed(list(response.readlines())):
+        #
+        if index >= 3:
+            #
+            if line.decode('utf-8')[0:2] == '45':
+                break
+                #
+            for i in range(0, 5):
+                #
+                if i == 0:
+                    yr_line = float('20' + line.decode('utf-8')[53:55]);
+                    #
+                    mo_line_string = line[50:53];
+                    mo_line = float(month_vec.index(mo_line_string) + 1);
+                    #
+                    day_line = float(line[48:50]);
+                    #
+                    F_10p7_value = float(line[57:59])
+                        
+                elif i == 1:
+                    yr_line = float('20' + line.decode('utf-8')[41:43]);
+                    #
+                    mo_line_string = line[38:41];
+                    mo_line = float(month_vec.index(mo_line_string) + 1);
+                    #
+                    day_line = float(line[36:38]);
+                    #
+                    F_10p7_value = float(line[45:47])
+                elif i == 2:
+                    yr_line = float('20' + line.decode('utf-8')[29:31]);
+                    #
+                    mo_line_string = line[26:29];
+                    mo_line = float(month_vec.index(mo_line_string) + 1);
+                    #
+                    day_line = float(line[24:26]);
+                    #
+                    F_10p7_value = float(line[33:35])
+                elif i == 3:
+                    yr_line = float('20' + line.decode('utf-8')[17:19]);
+                    #
+                    mo_line_string = line[14:17];
+                    mo_line = float(month_vec.index(mo_line_string) + 1);
+                    #
+                    day_line = float(line[12:14]);
+                    #
+                    F_10p7_value = float(line[21:23])
+                else:
+                    yr_line = float('20' + line.decode('utf-8')[5:7]);
+                    #
+                    mo_line_string = line[2:5];
+                    mo_line = float(month_vec.index(mo_line_string) + 1);
+                    #
+                    day_line = float(line[0:2]);
+                    #
+                    F_10p7_value = float(line[9:11]);
+                
+                #
+                if first_entry_flag == 1:
+                    #
+                    F_10p7_adj_future_array = np.array([yr_line, mo_line, day_line, F_10p7_value]);
+                    first_entry_flag = 0;
+                    #
+                else:
+                    F_10p7_adj_future_array = np.vstack((F_10p7_adj_future_array, [yr_line, mo_line,\
+                                       day_line, F_10p7_value]));
+                    #
+        index = index + 1;
+            #
+        #
     #
     # Pull historical information from the current and previous two quarters
     # Note: I am assuming that the ftp site only keeps four quarters up at any 
@@ -142,12 +224,12 @@ def pull_NOAA_F_10p7(yr, mon):
                                day_line, F_10p7_value]));
         #
     #
-    return F_10p7_obs_array
+    return F_10p7_obs_array, F_10p7_adj_future_array
 
 yr = 2018;
 mon = 9;
 
-F_10p7_obs_array = pull_NOAA_F_10p7(yr, mon)
+F_10p7_obs_array, F_10p7_adj_future_array = pull_NOAA_F_10p7(yr, mon)
         
         
     
